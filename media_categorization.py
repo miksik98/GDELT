2c8bfc316562ea641ba2cc891556dfac699fd18d
Jan <jan.ilczuk@gmail.com>
2021-06-05 21:13:29 +0200

    Add more code

--- media_categorization.py
+++ media_categorization.py
@@ -2,6 +2,7 @@ import pandas as pd
 import gdelt
 import numpy as np
 import csv
+from sklearn.tree import DecisionTreeClassifier
 
 
 class Country:
@@ -28,12 +29,30 @@ class Country:
             gov_attitude = 'anti'
         return gov_attitude
 
+    def get_avg_tone(self, dataset, media_addr):
+        res = dataset[(dataset['Actor1Code'] == 'GOV') & (dataset['Actor1Geo_CountryCode'] == self.gdeltCode) & (
+            dataset['SOURCEURL'].str.contains(media_addr))]
+        tone = np.sum(res['AvgTone'])
+        return tone
+
+    def get_num_sources(self, dataset, media_addr):
+        res = dataset[(dataset['Actor1Code'] == 'GOV') & (dataset['Actor1Geo_CountryCode'] == self.gdeltCode) & (
+            dataset['SOURCEURL'].str.contains(media_addr))]
+        return np.sum(res['NumSources'])
+
+    def get_root_events(self, dataset, media_addr):
+        res = dataset[(dataset['Actor1Code'] == 'GOV') & (dataset['Actor1Geo_CountryCode'] == self.gdeltCode) & (
+            dataset['SOURCEURL'].str.contains(media_addr))]
+        return len(res[res['IsRootEvent'] == 1])
+
     def get_reliability(self, dataset, media_addr):
         res = dataset[dataset['SOURCEURL'].str.contains(media_addr)]
         if len(res) == 0:
             return 1.0
-        notRootNumber = len(res[(res['NumSources'] < 4) & (res['IsRootEvent'] == 0)])
-        rootNumber = len(res[(res['NumSources'] < 4) & (res['IsRootEvent'] == 1)])
+        notRootNumber = len(
+            res[(res['NumSources'] < 4) & (res['IsRootEvent'] == 0)])
+        rootNumber = len(res[(res['NumSources'] < 4)
+                             & (res['IsRootEvent'] == 1)])
         notReliableArticlesNumber = 0.5 * notRootNumber + rootNumber
         if notReliableArticlesNumber / len(res) > 0.75:
             return 'not_reliable'
@@ -52,13 +71,16 @@ germany = Country('DE', 'de')
 india = Country('IN', 'in')
 brazil = Country('BR', 'br')
 usa = Country('US', 'us')
+# usa1 = Country('US', 'com')
 
 countries = [poland, russia, china, japan, germany, india, brazil, usa]
+# countries = [usa]
 
 
 def media_categorization(csv_name, date_range):
     with open(csv_name, 'w', newline='') as f:
-        fieldnames = ['media', 'government_attitude', 'reliability']
+        fieldnames = ['media', 'government_attitude',
+                      'reliability', 'avg_tone', 'root_events', 'num_sources']
         writer = csv.DictWriter(f, fieldnames=fieldnames)
         writer.writeheader()
         pd.set_option('max_columns', None)
@@ -66,15 +88,22 @@ def media_categorization(csv_name, date_range):
         gd1 = gdelt.gdelt(version=1)
         dataset = gd1.Search(date_range,
                              table='events', output='pd')
+        print(dataset.columns)
 
         for country in countries:
             for mediaAddr in country.get_media_names(dataset):
-                gov_attitude = country.get_tone_to_gov(dataset, mediaAddr)
-                reliability = country.get_reliability(dataset, mediaAddr)
-                writer.writerow({'media': mediaAddr, 'government_attitude': gov_attitude, 'reliability': reliability})
+                if mediaAddr in ['www.bbc.com', 'www.cnn.com', 'www.foxnews.com'] or True:
+                    gov_attitude = country.get_tone_to_gov(dataset, mediaAddr)
+                    reliability = country.get_reliability(dataset, mediaAddr)
+                    avg_tone = country.get_avg_tone(dataset, mediaAddr)
+                    root_events = country.get_root_events(
+                        dataset, mediaAddr)
+                    num_sources = country.get_num_sources(dataset, mediaAddr)
+                    writer.writerow({'media': mediaAddr, 'government_attitude': gov_attitude,
+                                     'reliability': reliability, 'avg_tone': avg_tone, 'root_events': root_events, 'num_sources': num_sources})
             print(f"Country {country} loaded!")
         print("Done!")
 
 
 if __name__ == '__main__':
-    media_categorization("media1.csv", ['2021 Apr 1', '2021 Apr 17'])
+    media_categorization("media5.csv", ['2020 Jan 6', '2020 Feb 6'])
